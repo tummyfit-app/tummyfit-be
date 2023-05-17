ddce421185f087c4aae67d8fe799724f473c8dcf
raflyafrzl <comfortyourstep@gmail.com>
2023-05-17 20:26:25 +0700

    feat:delete builds folder

--- app.ts
+++ app.ts
@@ -6,11 +6,6 @@ import userRouter from "./routes/UserRoute";
 const app = express();
 
 app.use(express.json());
-app.get("/", (req, res) => {
-  res.json({
-    status: "success",
-  });
-});
 
 app.use("/api/v1", userRouter);
 
