aec0684da61d8fe0eeb63312fbdb40f767a11aa1
raflyafrzl <comfortyourstep@gmail.com>
2023-05-28 18:34:40 +0700

    feat:changing seed command

--- controllers/FoodController.ts
+++ controllers/FoodController.ts
@@ -3,6 +3,7 @@ import { Controller } from "../interfaces/Controller";
 import prisma from "../config/DatabaseConnection";
 import { IFoodService } from "../services/Foods/IFoodService";
 import wrapAsync from "../utils/CatchAsync";
+import authorizationMiddleware from "../middlewares/AuthorizationMiddleware";
 
 class FoodController implements Controller {
   router: Router = Router();
@@ -12,7 +13,11 @@ class FoodController implements Controller {
   }
 
   initialRouting() {
-    this.router.get(`${this.path}`, wrapAsync(this.select.bind(this)));
+    this.router.get(
+      `${this.path}`,
+      authorizationMiddleware,
+      wrapAsync(this.select.bind(this))
+    );
   }
 
   async select(req: Request, response: Response, next: NextFunction) {
